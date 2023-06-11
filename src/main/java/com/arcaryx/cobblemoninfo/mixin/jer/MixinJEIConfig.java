0369e26e6604f40d68f631e4907ea1450ff6e8cb
Arcaryx <arcaryx@gmail.com>
2023-06-11 17:48:43 -0400

    Add JER Pokemon drop integration

--- src/main/java/com/arcaryx/cobblemoninfo/mixin/jer/MixinJEIConfig.java
+++ src/main/java/com/arcaryx/cobblemoninfo/mixin/jer/MixinJEIConfig.java
@@ -1,16 +1,17 @@
-package com.arcaryx.cobblemoninfo.mixin;
+package com.arcaryx.cobblemoninfo.mixin.jer;
 
+import com.cobblemon.mod.common.api.drop.ItemDropEntry;
 import com.cobblemon.mod.common.api.pokemon.PokemonProperties;
 import com.cobblemon.mod.common.api.pokemon.PokemonSpecies;
-import com.cobblemon.mod.common.client.render.models.blockbench.pokemon.PokemonFloatingState;
-import com.cobblemon.mod.common.client.render.models.blockbench.repository.PokemonModelRepository;
+import jeresources.api.conditionals.LightLevel;
 import jeresources.api.drop.LootDrop;
 import jeresources.compatibility.CompatBase;
 import jeresources.entry.MobEntry;
 import jeresources.jei.JEIConfig;
 import jeresources.registry.MobRegistry;
+import kotlin.ranges.IntRange;
 import mezz.jei.api.registration.IRecipeRegistration;
-import net.minecraft.world.item.Items;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -21,35 +22,30 @@ public abstract class MixinJEIConfig {
     @Inject(method = "registerRecipes(Lmezz/jei/api/registration/IRecipeRegistration;)V",at=@At(value="HEAD"), remap = false, require = 0)
     private void registerRecipes(IRecipeRegistration registration, CallbackInfo ci) {
         var pokemonSpecies = PokemonSpecies.INSTANCE.getSpecies();
-        int j = 0; // TODO: Remove (testing)
         for (var species : pokemonSpecies) {
             var drops = species.getDrops().getEntries();
             var lootDrops = new LootDrop[drops.size()];
             for (var i = 0; i < lootDrops.length; i++) {
                 var drop = drops.get(i);
-                // TODO: Remove (testing)
-                lootDrops[i] = new LootDrop(Items.STICK.getDefaultInstance());
+                if (drop instanceof ItemDropEntry itemDrop) {
+                    var item = ForgeRegistries.ITEMS.getValue(itemDrop.getItem());
+                    if (item != null) {
+                        var range = itemDrop.getQuantityRange();
+                        if (range == null)
+                            range = new IntRange(itemDrop.getQuantity(), itemDrop.getQuantity());
+                        var chance = itemDrop.getPercentage() / 100;
+                        lootDrops[i] = new LootDrop(item.getDefaultInstance(), range.getFirst(), range.getLast(), chance);
+                    }
+                }
             }
 
             var entry = MobEntry.create(() -> {
-                // TODO: Figure out how to animate this model
+                // TODO: Figure out how to set the pose (not t-posing)
                 var properties = new PokemonProperties();
                 properties.setSpecies(species.getName());
-                var entity = properties.createEntity(CompatBase.getLevel());
-                var model = PokemonModelRepository.INSTANCE.getPoser(species.getResourceIdentifier(), entity.getAspects().get());
-                model.setupAnimStateful(entity, new PokemonFloatingState(), 0, 0, 0, 0, 0);
-                return model.getCurrentEntity();
-            }, lootDrops);
+                return properties.createEntity(CompatBase.getLevel());
+            }, LightLevel.any, 0, lootDrops);
             MobRegistry.getInstance().registerMob(entry);
-            // TODO: Remove (testing)
-            j++;
-            if (j == 99)
-                break;
         }
-
-
-
-
     }
-
 }
