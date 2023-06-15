5d97f86ab43399f51809897dd8425c00d7c73816
Arcaryx <arcaryx@gmail.com>
2023-06-14 20:13:41 -0400

    Remove logging spam

--- src/main/java/com/arcaryx/cobblemoninfo/mixin/jer/MixinModInfo.java
+++ src/main/java/com/arcaryx/cobblemoninfo/mixin/jer/MixinModInfo.java
@@ -1,6 +1,5 @@
 package com.arcaryx.cobblemoninfo.mixin.jer;
 
-import com.arcaryx.cobblemoninfo.CobblemonInfo;
 import jeresources.fabric.ModInfo;
 import net.fabricmc.loader.api.ModContainer;
 import net.minecraft.server.packs.FilePackResources;
@@ -22,9 +21,7 @@ public abstract class MixinModInfo {
     @Inject(method = "getPackResources()Lnet/minecraft/server/packs/PackResources;", at=@At(value="HEAD"), cancellable = true, require = 0)
     private void fixGetPackResources(CallbackInfoReturnable<PackResources> cir) {
         // TODO: Temp fix until I have time to find a real solution and submit a PR to JER
-        CobblemonInfo.LOGGER.warn("Applying JER fix. Expect some error spam!");
         var path = this.modContainer.getRoot().toUri().toString().substring(12, this.modContainer.getRoot().toUri().toString().length() - 2);
-        CobblemonInfo.LOGGER.info(path);
         var file = new File(path);
         var pack = new FilePackResources(file);
         cir.setReturnValue(pack);
